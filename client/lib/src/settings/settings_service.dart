5675371e393deff393389fd5805d2b5699c57748
Bjarke Bondo Andersen <bba@orbit.online>
2023-01-30 21:30:23 +0100

    client: persist selected theme

--- client/lib/src/settings/settings_service.dart
+++ client/lib/src/settings/settings_service.dart
@@ -1,4 +1,6 @@
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -7,11 +9,18 @@ import 'package:flutter/material.dart';
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
   /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  Future<ThemeMode> themeMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? themeStr = prefs.getString('theme');
+    ThemeMode? themeMode = themeStr == null
+        ? null
+        : EnumToString.fromString(ThemeMode.values, themeStr);
+    return themeMode ?? ThemeMode.system;
+  }
 
   /// Persists the user's preferred ThemeMode to local or remote storage.
   Future<void> updateThemeMode(ThemeMode theme) async {
-    // Use the shared_preferences package to persist settings locally or the
-    // http package to persist settings over the network.
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('theme', EnumToString.convertToString(theme));
   }
 }
