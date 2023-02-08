56aea310e409ddfc84f6dc71d4ee7d85f1eb21f1
Bjarke Bondo Andersen <bba@orbit.online>
2023-02-08 21:12:29 +0100

    client: update intl system locale on change

--- client/lib/src/settings/settings_controller.dart
+++ client/lib/src/settings/settings_controller.dart
@@ -1,60 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'settings_service.dart';
 
-/// A class that many Widgets can interact with to read user settings, update
-/// user settings, or listen to user settings changes.
-///
-/// Controllers glue Data Services to Flutter Widgets. The SettingsController
-/// uses the SettingsService to store and retrieve user settings.
 class SettingsController with ChangeNotifier {
   SettingsController(this._settingsService);
 
-  // Make SettingsService a private variable so it is not used directly.
   final SettingsService _settingsService;
 
-  // Make ThemeMode a private variable so it is not updated directly without
-  // also persisting the changes with the SettingsService.
   late ThemeMode _themeMode;
-
-  // Allow Widgets to read the user's preferred ThemeMode.
   ThemeMode get themeMode => _themeMode;
 
   late Locale? _locale;
   Locale? get locale => _locale;
 
-  /// Load the user's settings from the SettingsService. It may load from a
-  /// local database or the internet. The controller only knows it can load the
-  /// settings from the service.
   Future<void> loadSettings() async {
     _themeMode = await _settingsService.themeMode();
     _locale = await _settingsService.locale();
-
-    // Important! Inform listeners a change has occurred.
+    Intl.systemLocale = _locale.toString();
     notifyListeners();
   }
 
-  /// Update and persist the ThemeMode based on the user's selection.
   Future<void> updateThemeMode(ThemeMode? newThemeMode) async {
     if (newThemeMode == null) return;
-
-    // Do not perform any work if new and old ThemeMode are identical
     if (newThemeMode == _themeMode) return;
-
-    // Otherwise, store the new ThemeMode in memory
     _themeMode = newThemeMode;
-
-    // Important! Inform listeners a change has occurred.
     notifyListeners();
-
-    // Persist the changes to a local database or the internet using the
-    // SettingService.
     await _settingsService.updateThemeMode(newThemeMode);
   }
 
   Future<void> updateLocale(Locale? newLocale) async {
     if (newLocale == _locale) return;
     _locale = newLocale;
+    Intl.systemLocale = newLocale.toString();
     notifyListeners();
     await _settingsService.updateLocale(newLocale);
   }
