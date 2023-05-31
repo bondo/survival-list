6a506e0b5078f9e72ddd22ed500e7b703174b3b2
Bjarke Bondo Andersen <bba@orbit.online>
2023-05-31 22:08:23 +0200

    Client: Display deadline in list tile subtitle

--- client/lib/schedule/widgets/schedule_list_tile.dart
+++ client/lib/schedule/widgets/schedule_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:survival_list/home/home.dart';
+import 'package:survival_list/l10n/l10n.dart';
 import 'package:survival_list_repository/survival_list_repository.dart';
 
 class ItemListTile extends StatelessWidget {
@@ -20,8 +22,12 @@ class ItemListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     final theme = Theme.of(context);
     final captionColor = theme.textTheme.bodySmall?.color;
+    final avatar = Avatar(
+      photo: (item.responsible ?? viewerPerson)?.pictureUrl,
+    );
 
     return Dismissible(
       key: Key('todoListTile_dismissible_${item.id}'),
@@ -54,16 +60,17 @@ class ItemListTile extends StatelessWidget {
                       ),
               ),
             ),
-            Avatar(
-              photo: (item.responsible ?? viewerPerson)?.pictureUrl,
-            ),
           ],
         ),
-        // subtitle: Text(
-        //   item.description,
-        //   maxLines: 1,
-        //   overflow: TextOverflow.ellipsis,
-        // ),
+        subtitle: item.endDate == null
+            ? null
+            : Expanded(
+                child: Text(
+                  DateFormat.yMMMMEEEEd(l10n.localeName).format(item.endDate!),
+                  maxLines: 1,
+                  style: const TextStyle(overflow: TextOverflow.ellipsis),
+                ),
+              ),
         leading: Checkbox(
           shape: const ContinuousRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(8)),
@@ -73,7 +80,16 @@ class ItemListTile extends StatelessWidget {
               ? null
               : (value) => onToggleCompleted!(value!),
         ),
-        trailing: onTap == null ? null : const Icon(Icons.chevron_right),
+        trailing: onTap == null
+            ? avatar
+            : FittedBox(
+                child: Row(
+                  children: [
+                    avatar,
+                    const Icon(Icons.chevron_right),
+                  ],
+                ),
+              ),
       ),
     );
   }
